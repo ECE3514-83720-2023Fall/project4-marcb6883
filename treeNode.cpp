0877cd7089a73f7b72af0a1e5288920f0fe215ad
Marc-Anthony Barsoum <marcb@vt.edu>
2023-12-09 18:16:06 -0500

    part 1

--- treeNode.cpp
+++ treeNode.cpp
@@ -5,58 +5,62 @@
 
 
 treeNode::treeNode() {
-	
+	rtEntry = { "", -1 }; // initialize with base value
+	leftChildPtr = nullptr;	
+	rightChildPtr = nullptr;
 }
 
 treeNode::treeNode(const std::string netId_in,  int port_in) {
+	rtEntry = { netId_in, port_in }; // constructor with init input
+	leftChildPtr = nullptr;
+	rightChildPtr = nullptr;
 	
 }
 
 void treeNode::setNetId(const std::string netId_in) {
 
-	
+	rtEntry.netId = netId_in; // use equal operator
 
 }
 void treeNode::setPort(const int port_in) {
 
-	
+	rtEntry.port = port_in; // use = operator
 }
 
 
 
 std::string treeNode::getNetId() const {
 	
-	return "";
+	return rtEntry.netId; // access the private member to return
 }
  int treeNode::getPort() const {
-	 return -1;
+	 return rtEntry.port; // access private port member to return
 	
 }
  RoutingEntry treeNode::getRoutingEntry() const
  {
-	 RoutingEntry s = { "",-1 };
-	 return s;
+	 return rtEntry;
  }
 bool treeNode::isLeaf() const {
-	
-	return false;
+	// shouldn't have ant children
+	return !leftChildPtr && !rightChildPtr;
 
 }
 
 std::shared_ptr<treeNode> treeNode::getLeftChildPtr() const {
 
-	return nullptr;
+	return leftChildPtr;
 	
 }
 std::shared_ptr<treeNode>  treeNode::getRightChildPtr() const {
-	return nullptr;
+	return rightChildPtr;
 }
 
 void treeNode::setLeftChildPtr(std::shared_ptr<treeNode> leftPtr) {
-	
+	leftChildPtr = leftPtr;
 }
 
 void treeNode::setRightChildPtr(std::shared_ptr<treeNode> rightPtr) {
-	
+	rightChildPtr = rightPtr;
 
 }
